a70c0978fed8bf23dd0916773122938f17dd5859
hyayi <98936899+hyayi@users.noreply.github.com>
2022-11-13 14:25:04 +0900

    Update changer_ex_s101_512x512_40k_levircd.py

--- configs/changer/changer_ex_s101_512x512_40k_levircd.py
+++ configs/changer/changer_ex_s101_512x512_40k_levircd.py
@@ -1,3 +1,4 @@
 _base_ = './changer_s50_512x512_40k_levircd.py'
 
 model = dict(backbone=dict(depth=101, stem_channels=128))
+load_from = '/home/lab/inseo/change/ChangerEx_s101-512x512_40k_levircd_20220710-082722'
